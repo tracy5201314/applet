0f9311ecee7ef8f950c75251d4ee4e30ae0bb460
tracy <351782450@qq.com>
2018-11-29 17:26:19 +0800

    新增乘机人说明弹窗，新增乘机人数量弹窗，日期选择增加判断（返程日期不能等于小于去程日期）

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,6 @@
 //获取应用实例
 const app = getApp()
 const date = new Date();
-// console.log(startDate.getDate())
 const startDate = date.getFullYear() + "-" + (date.getMonth()+1) + '-' + date.getDate()
 const week = (i) => {
   switch (i) {
@@ -30,21 +29,23 @@ const week = (i) => {
   }
 }
 
-console.log(startDate)
-
 Page({
   data: {
-    motto: '这是自己测试的小程序',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    srcArr:[1,2,3],
     navH:null,
     active:true,
-    returnDateStatus:true
+    returnDateStatus:true,
+    popupTipStatus:false,
+    addPassengersStatus:false,
+    adultNum:0,
+    childrenNum:0,
+    babyNum:0,
+    outTime:null,
+    returnTime:null
   },
   onPageScroll: function (e) {
-    console.log(e)
     if(e.scrollTop>5){
       this.setData({
         bgColor:"rgba(0,0,0,1)"
@@ -63,7 +64,8 @@ Page({
     //设置默认日期为当前时间
     this.setData({
       startDate:startDate,
-      date: (date.getMonth() + 1) + '月' + date.getDate() + "日" + "   " + week(date.getDay())
+      date: (date.getMonth() + 1) + '月' + date.getDate() + "日" + "   " + week(date.getDay()),
+      outTime:date.getTime()
     })
     //获取用户的信息
     if (app.globalData.userInfo) {
@@ -118,19 +120,29 @@ Page({
   },
   //日期选择
   bindDateChange: function (e) {
-    console.log(e)
     if(e.currentTarget.dataset.time==0){
       let selectDate = new Date(e.detail.value)
-      let dateValue = (selectDate.getMonth() + 1) + '月' + selectDate.getDate() + "日" + "   " + week(selectDate.getDay())
+      let dateValue = (selectDate.getMonth() + 1) + '月' + selectDate.getDate() + "日" + "   " + week(selectDate.getDay())        
       this.setData({
-        date:dateValue
+        date:dateValue,
+        outTime:selectDate.getTime()
       })
     }else{
       let selectDate = new Date(e.detail.value)
       let dateValue = (selectDate.getMonth() + 1) + '月' + selectDate.getDate() + "日" + "   " + week(selectDate.getDay())
       this.setData({
-        returnDateStatus:false,
-        date1: dateValue
+        returnTime:selectDate.getTime()
+      })
+      if(this.data.outTime>=this.data.returnTime){
+        wx.showToast({
+          title: '返程日期错误！',
+          icon:"none"
+        })
+        return false
+      }
+      this.setData({
+        returnDateStatus: false,
+        date1: dateValue,
       })
     }
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -140,7 +152,88 @@ Page({
     this.setData({
       returnDateStatus:true
     })
-  }
+  },
+  //弹出说明弹窗
+  openPopupTip:function(){
+    this.setData({
+      popupTipStatus:true,
+      isPopupShow:true
+    })    
+  },
+  //关闭说明弹窗
+  closePopupTip: function () {
+    this.setData({
+      popupTipStatus: false,
+      isPopupShow:false
+    })
+  },
+  //阻止page滑动
+  preventTouchMove:function(){},
+  // 增加和减少乘机人数
+  minusNum:function(e){
+    switch (e.currentTarget.dataset.num) {
+      case "0":
+        if(this.data.adultNum <=0) return false
+        let n1 = this.data.adultNum;
+        n1--;
+        this.setData({
+          adultNum: n1
+        })
+        break;
+      case "1":
+        if (this.data.childrenNum <= 0) return false
+        let n2 = this.data.childrenNum;
+        n2--;
+        this.setData({
+          childrenNum: n2
+        })
+        break;
+      case "2":
+        if (this.data.babyNum <= 0) return false
+        let n3 = this.data.babyNum;
+        n3--;
+        this.setData({
+          babyNum: n3
+        })
+        break;
+    }
+  },
+  addNum: function (e) {
+    switch (e.currentTarget.dataset.num){
+      case "0":
+        let n1 = this.data.adultNum;
+        n1++;
+        this.setData({
+          adultNum:n1
+        })
+        break;
+      case "1":
+        let n2 = this.data.childrenNum;
+        n2++;
+        this.setData({
+          childrenNum: n2
+        })
+        break;
+      case "2":
+        let n3 = this.data.babyNum;
+        n3++;
+        this.setData({
+          babyNum: n3
+        })
+        break;
+    }
+  },
+  //关闭打开选择乘机人弹窗
+  openAddPassengersPopup:function(){
+    this.setData({
+      addPassengersStatus:true
+    })
+  },
+  closeAddPassengersPopup: function () {
+    this.setData({
+      addPassengersStatus: false
+    })
+  },
 })
 
 
